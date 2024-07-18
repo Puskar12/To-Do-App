51bc24ff120e4486b98e977f6c847694635d891a
Puskar Das <puskardas0318@gmail.com>
2024-07-18 13:22:20 +0530

    first commit

---
+++ todo.js
@@ -0,0 +1,38 @@
+let todoList = [
+  {
+    item:'Milk niye aste hobe',
+    dueDate:'02/07/2024',
+  },
+  {
+    item:'College jete hobe',
+    dueDate:'03/07/2024',
+  }
+];
+displayItems();
+function addTodo(){
+  let inputElement = document.querySelector('#todoInput');
+  let dateElement = document.querySelector('#todoDate')
+  let todoItem = inputElement.value ;
+  let todoDate = dateElement.value ;
+  todoList.push({item:todoItem, dueDate:todoDate});
+  inputElement.value = '';
+  dateElement.value = '';
+  displayItems();
+  
+}
+
+function displayItems(){
+  let containerElement = document.querySelector('.todoContainer');
+  let newHtml = '';
+  for (i = 0; i < todoList.length ; i++ ){
+    let {item,dueDate} = todoList[i];
+    newHtml += `
+    
+    <span>${item}</span>
+    <span>${dueDate}</span>
+    <button id = "deleteButton" onclick = "todoList.splice(${[i]}, 1);
+    displayItems();">Delete</button>
+     `;
+    }
+    containerElement.innerHTML= newHtml;
+  }
